4130577b06d0576b29ed1526928dc3a2be417d8d
Arnau Puiggené <89878398+XpuiggeX@users.noreply.github.com>
2025-10-24 20:01:19 +0200

    Add files via upload

--- src/baseNoStates/DirectoryUsers.java
+++ src/baseNoStates/DirectoryUsers.java
@@ -30,12 +30,9 @@ public final class DirectoryUsers {
             LocalTime.of(0, 0)
     );
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
     UserGroup employees = new UserGroup("employees",
-              Arrays.asList(Actions.UNLOCK_SHORTLY),
+              Arrays.asList(Actions.UNLOCK_SHORTLY, Actions.CLOSE, Actions.OPEN),
             LocalDateTime.of(2025, 9, 1, 0, 0),
               LocalDateTime.of(2026, 3, 1, 0, 0),
               Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
@@ -46,7 +43,7 @@ public final class DirectoryUsers {
 
 
     UserGroup managers = new UserGroup("managers",
-              Arrays.asList(Actions.LOCK, Actions.UNLOCK, Actions.UNLOCK_SHORTLY),
+              Arrays.asList(Actions.LOCK, Actions.UNLOCK, Actions.UNLOCK_SHORTLY, Actions.CLOSE, Actions.OPEN),
               LocalDateTime.of(2025, 9, 1, 0, 0),
               LocalDateTime.of(2026, 3, 1, 0, 0),
               Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
@@ -55,70 +52,9 @@ public final class DirectoryUsers {
               LocalTime.of(20, 0)
     );
 
-=======
-<<<<<<< HEAD
-      UserGroup employees = new UserGroup("employees",
-                Arrays.asList(Actions.UNLOCK_SHORTLY, Actions.OPEN, Actions.CLOSE),
-=======
-
-=======
-<<<<<<< HEAD
-      UserGroup employees = new UserGroup("employees",
-                Arrays.asList(Actions.UNLOCK_SHORTLY, Actions.OPEN, Actions.CLOSE),
-=======
-
->>>>>>> Stashed changes
-=======
-<<<<<<< HEAD
-      UserGroup employees = new UserGroup("employees",
-                Arrays.asList(Actions.UNLOCK_SHORTLY, Actions.OPEN, Actions.CLOSE),
-=======
-
->>>>>>> Stashed changes
-    UserGroup employees = new UserGroup("employees",
-              Arrays.asList(Actions.UNLOCK_SHORTLY),
-            LocalDateTime.of(2025, 9, 1, 0, 0),
-              LocalDateTime.of(2026, 3, 1, 0, 0),
-              Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
-                      DayOfWeek.THURSDAY, DayOfWeek.FRIDAY),
-              LocalTime.of(9, 0),
-              LocalTime.of(17, 0)
-    );
-
-
-    UserGroup managers = new UserGroup("managers",
-              Arrays.asList(Actions.LOCK, Actions.UNLOCK, Actions.UNLOCK_SHORTLY),
->>>>>>> b9a1a1ffb6097522703aa0e2beb84a31111a93bf
-              LocalDateTime.of(2025, 9, 1, 0, 0),
-              LocalDateTime.of(2026, 3, 1, 0, 0),
-              Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
-                      DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY),
-              LocalTime.of(8, 0),
-              LocalTime.of(20, 0)
-    );
-
-<<<<<<< HEAD
-      UserGroup managers = new UserGroup("managers",
-                Arrays.asList(Actions.LOCK, Actions.UNLOCK, Actions.UNLOCK_SHORTLY, Actions.OPEN, Actions.CLOSE),
-                LocalDateTime.of(2025, 9, 1, 0, 0),
-                LocalDateTime.of(2026, 3, 1, 0, 0),
-                Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
-                        DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY),
-                LocalTime.of(8, 0),
-                LocalTime.of(20, 0)
-      );
-=======
->>>>>>> b9a1a1ffb6097522703aa0e2beb84a31111a93bf
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     UserGroup admin = new UserGroup("admin",
-              Arrays.asList(Actions.LOCK, Actions.UNLOCK, Actions.UNLOCK_SHORTLY),
+              Arrays.asList(Actions.LOCK, Actions.UNLOCK, Actions.UNLOCK_SHORTLY, Actions.CLOSE, Actions.OPEN),
             LocalDateTime.of(2024, 1, 1, 0, 0),
               LocalDateTime.of(2100, 1, 1, 0, 0),
               Arrays.asList(DayOfWeek.values()),
@@ -126,32 +62,6 @@ public final class DirectoryUsers {
               LocalTime.MAX
     );
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
-      UserGroup admin = new UserGroup("admin",
-                Arrays.asList(Actions.LOCK, Actions.UNLOCK, Actions.UNLOCK_SHORTLY, Actions.OPEN, Actions.CLOSE),
-              LocalDateTime.of(2024, 1, 1, 0, 0),
-                LocalDateTime.of(2100, 1, 1, 0, 0),
-                Arrays.asList(DayOfWeek.values()),
-                LocalTime.MIN,
-                LocalTime.MAX
-      );
-=======
->>>>>>> b9a1a1ffb6097522703aa0e2beb84a31111a93bf
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     // user sin privilegios ("blank")
     User bernat = new User("Bernat", "12345");
@@ -177,44 +87,12 @@ public final class DirectoryUsers {
     users.add(manel);
     users.add(marta);
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
-      // managers
-      User manel = new User("Manel", "95783");
-      User marta = new User("Marta", "05827");
-      userGroups.get(2).addUser(manel);
-      userGroups.get(2).addUser(marta);
-      users.add(manel);
-      users.add(marta);
-
-      // admin
-      User ana = new User("Ana", "11343");
-      userGroups.get(3).addUser(ana);
-      users.add(ana);
-  }
-  //Encuentra el user segun su credential
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     // admin
     User ana = new User("Ana", "11343");
     admin.addUser(ana);
     users.add(ana);
 }
 
->>>>>>> b9a1a1ffb6097522703aa0e2beb84a31111a93bf
   public static User findUserByCredential(String credential) {
     for (User user : users) {
       if (user.getCredential().equals(credential)) {
